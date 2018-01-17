cd557d45f3fe3caee62cda32517bd153cf9773f3
Sam Hedges <Sam@Sams-MacBook-Pro.local>
2018-01-17 18:15:05 +0000

    added second letter map in constructor and private member

--- transformer.h
+++ transformer.h
@@ -4,11 +4,12 @@
 
 class Transformer {
 public:
-    Transformer();
+    Transformer(const char letters[], const char rLetters[]);
     char transform(char c, int offset);
 
 private:
     char map[26];
+    char rMap[26];
 };
 
 #endif // TRANSFORMER_H
