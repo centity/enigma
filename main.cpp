a078d5a5d36f98fcf2661d989029746c67164ad9
Sam Hedges <Sam@Sams-MacBook-Pro.local>
2018-01-17 23:17:06 +0000

    absorbed transform methods into enigma transform

--- main.cpp
+++ main.cpp
@@ -6,57 +6,17 @@
 
 int main()
 {
-    //Enigma enigma("abcdefghijklmnopqrstuvw", )
-    int lRot, mRot, rRot;
-    int ref;
-    char lStart, mStart, rStart;
-    int lOffset, mOffset, rOffset;
-    char chr;
+    // Placeholders
+    int lRotNum = 0, mRotNum = 0, rRotNum = 0;
+    char lStartChar = 'a', mStartChar = 'a', rStartChar = 'a';
+    int refNum = 0;
 
-    char rotMaps[5][27] = {
-        "ekmflgdqvzntowyhxuspaibrcj",
-        "ajdksiruxblhwtmcqgznpyfvoe",
-        "bdfhjlcprtxvznyeiwgakmusqo",
-        "esovpzjayquirhxlnftgkdcmwb",
-        "vzbrgityupsdnhlxawmjqofeck"};
+    Enigma enigma("abcdefghijklmnopqrstuvwxyz", lRotNum, lStartChar, mRotNum,
+                  mStartChar, rRotNum, rStartChar, refNum);
 
-    char refMaps[2][27] = {
-        "yruhqsldpxngokmiebfzcwvjat",
-        "fvpjiaoyedrzxwgctkuqsbnmhl"};
-
-    char plugMap[27] = {
-        "abcdefghijklmnopqrstuvwxyz"};
-
-
-
-    // To become user defined
-    lRot = 2, mRot = 1, rRot = 0;
-    ref = 0;
-    lStart = 'a', mStart = 'a', rStart = 'a';
-    char input[] = "test";
-
-    // Set the offset based on the start position of each rotor
-    lOffset = lStart - 'a', mOffset = mStart - 'a', rOffset = rStart - 'a';
-
-    Rotor lRotor(rotMaps[lRot], lOffset);
-    Rotor mRotor(rotMaps[mRot], mOffset);
-    Rotor rRotor(rotMaps[rRot], rOffset);
-    Reflector reflector(refMaps[ref]);
-    Plugboard plugboard(plugMap);
-
-    int i = 0;
-    while (i < 4){
-        chr = input[i];
-        chr = plugboard.transform(chr);
-        chr = lRotor.transform(mRotor.transform(rRotor.transform(chr)));
-        chr = reflector.transform(chr);
-        std::cout << chr;
-
-        i++;
-
-
-        rRotor.rotate();
-        if (i % 26 == 0) {mRotor.rotate();} // Every 26 turns of rRotor
-        if (i % 676 == 0) {lRotor.rotate();} // Every 26 turns of mROtor
+    char test[] = "test";
+    for (int i = 0; i < 4; i++){
+        test[i] = enigma.transform(test[i]);
+        std::cout << test[i];
     }
 }
