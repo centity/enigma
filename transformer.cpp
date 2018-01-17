f354bdc69411eeecdc71fd73d0c88f21662b553b
Sam Hedges <Sam@Sams-MacBook-Pro.local>
2018-01-17 17:36:48 +0000

    transformer stuff

--- transformer.cpp
+++ transformer.cpp
@@ -1,13 +1,13 @@
 #include "transformer.h"
 
-Transformer::Transformer(const char letters[], ) {
+Transformer::Transformer(const char letters[]) {
     for (int i = 0; i < 26; i++){
         map[i] = letters[i];
     }
 }
 
 Transformer::transform(char c, int offset){
-    return map[c + offset  % 26];
+    return map[(c + offset)  % 26];
 }
 
 
